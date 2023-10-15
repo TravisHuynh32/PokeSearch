4419e9d15c930a166465367b8064199db5c469f1
Travis Huynh <118507351+DuhBoss32@users.noreply.github.com>
2023-10-14 23:22:08 -0700

    Pokemon Move Summary using PokeAPI

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import webbrowser
 
 def fetch_pokemon_id(name):
     poke = move(name) 
-    print("ID:")
-    print("-------------")
+    print("Summary:")
+    print("-------------------------------------------------------------------------------------------------------")
     print("The " + str(poke.type) + " type move " + name + " is a " + str(poke.damage_class) + " type move that has an id of: " + str(poke.id))
     print("This move was first introduced in " + str(poke.generation))
     print("")
@@ -16,15 +16,15 @@ def fetch_pokemon_id(name):
 def fetch_all_pokemon_move(name): 
     poke = move(name)
     print("List of Pokemon that can aquire this move!")
-    print("-------------")
+    print("-------------------------------------------------------------------------------------------------------")
     print(*poke.learned_by_pokemon, sep = ", ")
     print("")
 
 
 def fetch_pokemon_power(name):
     poke = move(name) 
-    print("Power:")
-    print("-------------")
+    print("Combat:")
+    print("-------------------------------------------------------------------------------------------------------")
     if poke.power < 100: 
         print(name + " has " + str(poke.power) + " power" + " and has " + str(poke.pp) + " pp")
         print("The power on this move is less than 100, wow so shit haha!")
@@ -32,7 +32,6 @@ def fetch_pokemon_power(name):
         print("")
         compare_eq = (poke.power / 100) * 100
         print(name + " is " + str(compare_eq) + "% of Earthquake(ID: 84) in comparison to its power (because earthquake is op)")
-        print("") 
     elif poke.power == 100: 
         print(name + " has " + str(poke.power) + " power" + " and has " + str(poke.pp) + " pp")
         print("WOW! I have much respect for this move! It has power that has the same power as earthquake OMG!!!")
@@ -40,7 +39,6 @@ def fetch_pokemon_power(name):
         print("")
         compare_eq = (poke.power / 100) * 100
         print(name + " is " + str(compare_eq) + "% of Earthquake(ID: 84) in comparison to its power (because earthquake is op)")
-        print("") 
     else: 
         print(name + " has " + str(poke.power) + " power" + " and has " + str(poke.pp) + " pp")
         print("Bruh, ain't no way its better than stab eq, how does it have more power :( ")
@@ -48,10 +46,11 @@ def fetch_pokemon_power(name):
         print("")
         compare_eq = (poke.power / 100) * 100
         print(name + " is " + str(compare_eq) + "% of Earthquake(ID: 84) in comparison to its power (because earthquake is op)")
-        print("")
+    print("-------------------------------------------------------------------------------------------------------")
+    print("")
 
 def fetch_move_info(name):
-    print("Fetching " + name + "...........\n")
+    print("Fetching " + name + ".......\n")
     fetch_all_pokemon_move(name)
     fetch_pokemon_id(name)
     fetch_pokemon_power(name)
@@ -77,6 +76,7 @@ def main():
 
         print("")
         print("Would you like to learn more about a specific pokemon move? Or You can enter nothing to exit this program!")
+        print("")
         print("-------------------------------------------------------------------------------------------------------")
 
 if __name__ == '__main__':
