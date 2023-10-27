ba8ae3fe0def34806e028f29c02751e94615f04e
Travis Huynh <118507351+DuhBoss32@users.noreply.github.com>
2023-10-27 16:05:25 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from pokebase import move
 from tkinter import *
+from tkinter import messagebox
 import requests
+import os
 from PIL import Image, ImageTk
 
 # basic Pokemon Move Finder Program
 # Created by: Travis Huynh
 # Using Python GUI for interactive user experience! 
 
-def fetch_all_pokemon_move(name): 
-    poke = move(name)
-    result = "List of Pokemon that can acquire this move!\n"
-    result += "-------------------------------------------------------------------------------------------\n"
-    result += ", ".join(pokemon.name for pokemon in poke.learned_by_pokemon) + "\n"
-    return result
+def show_pokemon_list(learned_by_pokemon):
+    list_window = Toplevel(root)
+    list_window.title("Pokémon that can acquire this move")
+    
+    list_text = Text(list_window, wrap=WORD, width=40, height=10, font=("Arial", 12))
+    list_text.pack(padx=10, pady=10)
+
+    list_text.insert(END, "\n".join(learned_by_pokemon))
+
+def fetch_all_pokemon_move(name):
+    try:
+        poke = move(name)
+        learned_by_pokemon = [pokemon.name for pokemon in poke.learned_by_pokemon]
+        
+        user_response = messagebox.askyesno("Show Pokemon?", f"Do you want to see the list of {len(learned_by_pokemon)} Pokémon that can acquire this move?")
+        
+        if user_response:
+            show_pokemon_list(learned_by_pokemon)
+            return "The list of Pokémon has been displayed in a separate window.\n"
+        else:
+            return "You chose not to see the list of Pokémon.\n"
+    except Exception as e:
+        return f"Error fetching information: {e}"
+
 
 def fetch_pokemon_id(name):
     poke = move(name)
@@ -56,32 +76,24 @@ def fetch_move_info_gui(name):
         output_text.insert(END, f"Error: {e}")
         return
 
-    # Fetch move details
     output_text.insert(END, "Fetching " + name + ".......\n\n")
     output_text.insert(END, fetch_all_pokemon_move(name))
     output_text.insert(END, fetch_pokemon_id(name))
     output_text.insert(END, fetch_pokemon_power(name))
 
-    # Display move image
-    try:
-        sprite = Image.open(f"icons/{name.lower()}.png")
-        sprite = sprite.resize((100, 100))
-        sprite = ImageTk.PhotoImage(sprite)
-        sprite_label.config(image=sprite)
-        sprite_label.image = sprite
-    except Exception as e:
-        sprite_label.config(image=None)
-        print(f"Error loading sprite: {e}")
+def clear_output():
+    output_text.delete(1.0, END)
 
 def main_gui():
-    global root 
+    global root
     root = Tk()
     root.title("Pokemon Move Finder")
+    root.configure(bg="#add8e6")  # Set the background color to light blue
 
-    label = Label(root, text="Welcome to Pokemon Move Finder!\n Created by: Travis Huynh", font=("Arial", 16))
+    label = Label(root, text="Welcome to Pokemon Move Finder!\n Created By: Travis Huynh", font=("Calibri", 16), bg="#add8e6")
     label.pack(pady=10)
 
-    entry = Entry(root, font=("Arial", 12), width=30)
+    entry = Entry(root, font=("Calibri", 12), width=30)
     entry.pack(pady=10)
 
     def search_move():
@@ -89,21 +101,21 @@ def main_gui():
         if name:
             fetch_move_info_gui(name)
 
-    button = Button(root, text="Search Move", command=search_move, font=("Arial", 12))
+    button = Button(root, text="Search Move", command=search_move, font=("Calibri", 12), bg="#ffffff")
     button.pack(pady=10)
 
-    global output_text
-    output_text = Text(root, wrap=WORD, width=50, height=20, font=("Arial", 12))
-    output_text.pack(pady=10)
+    clear_button = Button(root, text="Clear Output", command=clear_output, font=("Calibri", 12), bg="#ffffff")
+    clear_button.pack(pady=10)
 
-    global sprite_label
-    sprite_label = Label(root)
-    sprite_label.pack(pady=10)
+    global output_text
+    output_text = Text(root, wrap=WORD, width=50, height=20, font=("Calibri", 12))
+    output_text.pack(pady=10)
 
     custom_font = ("Helvetica", 14)
     label.configure(font=custom_font)
     entry.configure(font=custom_font)
     button.configure(font=custom_font)
+    clear_button.configure(font=custom_font)
     output_text.configure(font=custom_font)
 
     root.mainloop()
